c264b24b16bc0b196155f305d5a1e8d8564d819e
n221199515 <hong_ronald@hotmail.com>
2018-03-07 14:32:33 -0600

    create HTML template, and alter html content using {{}}

--- first_app/views.py
+++ first_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+    my_dict={'insert_me':"hello! from views.py !"}
+    return render(request,'first_app/index.html', context=my_dict)
 
 # Create your views here.
